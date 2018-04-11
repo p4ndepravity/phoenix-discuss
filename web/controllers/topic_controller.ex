4bf7669dd3b1e256715331bdcc77981d09c3f644
Trent <trent.bullard@gmail.com>
2018-04-11 11:02:17 -0500

    implement feature security

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,6 +2,9 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
+
   def index(conn, _params) do
     topics = Repo.all Topic
     render conn, "index.html", topics: topics
@@ -13,7 +16,10 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset %Topic{}, topic
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
+
     case Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
@@ -49,5 +55,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You do not have permission to do that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
