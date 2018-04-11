44ad4f580bcd20a01b4501d2eb2a1395d0e9c99c
Trent <trent.bullard@gmail.com>
2018-04-11 08:20:51 -0500

    implement user model, implement github oauth backend

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "b83fe63c8ed813855627",
+  client_secret: "f6a5c22d3554f5d231f52bb4ea1ab48bc6b05464"
